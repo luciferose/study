ad70600a6a50e7ac1f2196946a58e39a603d54f8
Administrator <20139816@qq.com>
2019-03-26 09:10:02 +0800

    add vsearch4web.py

--- flask_test/vsearch4web.py
+++ flask_test/vsearch4web.py
@@ -23,4 +23,5 @@ def do_search() -> 'html' :
 def entry_page() -> 'html':
     return render_template('entry.html',the_title='welcome to search on teh web!')
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
