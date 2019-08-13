80e795b77e5d776311a26298d70ae023a6c47d97
Administrator <20139816@qq.com>
2019-08-13 13:41:28 +0800

    add crazy_lib.py koch.py lib.txt race.py turtle_test.py

---
+++ deque_test.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+
+if __name__ == '__main__':
+    with open('lib.txt') as f:
+        for line, prevlines in search(f, 'your', 5):
+            for pline in prevlines:
+                print(pline, end='')
+            print(line, end='')
+            print('-'*20)
