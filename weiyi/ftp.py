fe60bea3ea4863c2a210e75b0457e09c26bfa3d0
Administrator <20139816@qq.com>
2019-11-26 15:14:46 +0800

    add review bug in test/ftp.py

--- weiyi/ftp.py
+++ weiyi/ftp.py
@@ -1,12 +1,13 @@
 from ftplib import  FTP
 import time
-ip_list=['192.168.111.1']
+ip_list=['192.168.188.1']
 for ip in ip_list:
     ftp = FTP()
     timout = 600
     port = 21
     ftp.connect(ip, port, timout)
     ftp.login('bxsadmin', 'se12pa##1')
+    print(ip)
     print(ftp.welcome)
     bufsize = 1024
     filename = time.strftime("%Y %a %b %d %H:%M:%S", time.localtime())+'_'+ip+'_'+'vrpcfg.zip'
